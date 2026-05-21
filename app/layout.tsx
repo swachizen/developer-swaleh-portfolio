1fda9621c6b30d3480f2252c3a1cf556b31e1c80
Swachizen <swalehmohamed203@gmail.com>
2026-05-21 16:41:16 +0300

    Add vercel insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "react-hot-toast";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import "./globals.css";
 
